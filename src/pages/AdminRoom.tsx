7206a7361f51bc21f990059407d1b71dc25e5f2a
Erico Alexandre <ericoabs25@gmail.com>
2021-06-26 21:50:46 -0300

    Criação da função de likes

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,3 @@
-import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
@@ -19,36 +18,10 @@ type RoomParams = {
 export function AdminRoom() {
   const params = useParams<RoomParams>();
   const { user } = useAuth();
-  const [newQuestion, setNewQuestion] = useState('');
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
 
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (newQuestion.trim() === '') {
-      return;
-    }
-
-    if (!user) {
-      throw new Error('You must be logged in');
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar,
-      },
-      isHighlighted: false,
-      isAnswered: false,
-    };
-
-    await database.ref(`rooms/${roomId}/questions`).push(question);
-
-    setNewQuestion('');
-  }
 
   return (
     <div id="page-room">
